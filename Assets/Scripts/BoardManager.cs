fd63baf27bdcfd21d352334e9b5d03928693364e
mervezgin <mervezgin@gmail.com>
2025-01-09 23:37:55 +0300

    [ADD]setted game grid

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    [SerializeField] private Transform tilePrefab;
+    private int height = 14;
+    private int width = 12;
+    private void Start()
+    {
+        SetGameGrid();
+    }
+    private void SetGameGrid()
+    {
+        for (int y = 0; y < height; y += 2)
+        {
+            for (int x = 0; x < width; x += 2)
+            {
+                Transform tile = Instantiate(tilePrefab, new Vector3(x, y, 0), Quaternion.identity);
+                tile.parent = this.transform;
+            }
+        }
+    }
+}
